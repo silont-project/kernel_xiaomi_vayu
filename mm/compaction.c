49d2c691b905a233afa9c3d959c67ea48c5eb428
kdrag0n <dragon@khronodragon.com>
2018-12-07 20:42:08 -0800

    mm: compaction: boost CPU and DDR bus during background compaction
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Salllz <sal235222727@gmail.com>
    Change-Id: I6f3cc2dd52c01c90ff3886f043ff0d616c64173c

--- mm/compaction.c
+++ mm/compaction.c
@@ -27,6 +27,7 @@
 #include <linux/moduleparam.h>
 #include <linux/time.h>
 #include <linux/workqueue.h>
+#include <linux/devfreq_boost.h>
 #include "internal.h"
 
 #ifdef CONFIG_COMPACTION
@@ -2036,6 +2037,9 @@ static void kcompactd_do_work(pg_data_t *pgdat)
 	trace_mm_compaction_kcompactd_wake(pgdat->node_id, cc.order,
 							cc.classzone_idx);
 	count_compact_event(KCOMPACTD_WAKE);
+
+	devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW_DDR, 100);
+	devfreq_boost_kick_max(DEVFREQ_MSM_CPU_LLCCBW, 100);
 
 	for (zoneid = 0; zoneid <= cc.classzone_idx; zoneid++) {
 		int status;
