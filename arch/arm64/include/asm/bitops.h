346a152dcae3c98904da8858efd8417c229ff704
Yaroslav Furman <yaro330@gmail.com>
2021-01-08 14:26:35 +0300

    HACK: locking/atomics/arm64: use different implementation of bitops for LTO
    For some bizarre reason LTO seems to regress performance of bit operations
    pretty hard, resulting in callbench read time increase from ~5.5k ns to 8.9k ns
    regardless of toolchain used (Clang-10/GCC-10.2).
    
    'dd if=/dev/zero of=/dev/null count=10000000' results w/o this workaround:
    GCC LTO + bitops.S:	 2.656 s ±  0.018 s
    Clang LTO + bitops.S:	 2.754 s ±  0.047 s
    GCC + bitops.S:		 2.946 s ±  0.052 s
    Clang + bitops.S:	 3.012 s ±  0.047 s
    
    Adding a workaround improves non-LTO results and brings them to ~2.75 s mark.
    
    All tests ran with Android stopped using hyperfine with 3 warmup runs.
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- arch/arm64/include/asm/bitops.h
+++ arch/arm64/include/asm/bitops.h
@@ -16,15 +16,21 @@
 #ifndef __ASM_BITOPS_H
 #define __ASM_BITOPS_H
 
+#include <linux/atomic.h>
 #include <linux/compiler.h>
+#ifdef CONFIG_LTO
 #include <asm/barrier.h>
+#endif
 
 #ifndef _LINUX_BITOPS_H
 #error only <linux/bitops.h> can be included directly
 #endif
 
+#ifdef CONFIG_LTO
 /*
  * Little endian assembly atomic bitops.
+ * Use bitops.S variants with LTO to avoid
+ * a regression.
  */
 extern void set_bit(int nr, volatile unsigned long *p);
 extern void clear_bit(int nr, volatile unsigned long *p);
@@ -32,6 +38,66 @@ extern void change_bit(int nr, volatile unsigned long *p);
 extern int test_and_set_bit(int nr, volatile unsigned long *p);
 extern int test_and_clear_bit(int nr, volatile unsigned long *p);
 extern int test_and_change_bit(int nr, volatile unsigned long *p);
+#else
+/*
+ * Use rewritten atomics when LTO not enabled.
+ * To, again, avoid regression.
+ * This is a terrbile hack and I am ashamed.
+ */
+static inline void set_bit(unsigned int nr, volatile unsigned long *p)
+{
+	p += BIT_WORD(nr);
+	atomic_long_or(BIT_MASK(nr), (atomic_long_t *)p);
+}
+
+static inline void clear_bit(unsigned int nr, volatile unsigned long *p)
+{
+	p += BIT_WORD(nr);
+	atomic_long_andnot(BIT_MASK(nr), (atomic_long_t *)p);
+}
+
+static inline void change_bit(unsigned int nr, volatile unsigned long *p)
+{
+	p += BIT_WORD(nr);
+	atomic_long_xor(BIT_MASK(nr), (atomic_long_t *)p);
+}
+
+static inline int test_and_set_bit(unsigned int nr, volatile unsigned long *p)
+{
+	long old;
+	unsigned long mask = BIT_MASK(nr);
+
+	p += BIT_WORD(nr);
+	if (READ_ONCE(*p) & mask)
+		return 1;
+
+	old = atomic_long_fetch_or(mask, (atomic_long_t *)p);
+	return !!(old & mask);
+}
+
+static inline int test_and_clear_bit(unsigned int nr, volatile unsigned long *p)
+{
+	long old;
+	unsigned long mask = BIT_MASK(nr);
+
+	p += BIT_WORD(nr);
+	if (!(READ_ONCE(*p) & mask))
+		return 0;
+
+	old = atomic_long_fetch_andnot(mask, (atomic_long_t *)p);
+	return !!(old & mask);
+}
+
+static inline int test_and_change_bit(unsigned int nr, volatile unsigned long *p)
+{
+	long old;
+	unsigned long mask = BIT_MASK(nr);
+
+	p += BIT_WORD(nr);
+	old = atomic_long_fetch_xor(mask, (atomic_long_t *)p);
+	return !!(old & mask);
+}
+#endif
 
 #include <asm-generic/bitops/builtin-__ffs.h>
 #include <asm-generic/bitops/builtin-ffs.h>
