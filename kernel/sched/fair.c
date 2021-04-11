05d693f684b92706282960668ef3c33c2b9861cf
Danny Lin <danny@kdrag0n.dev>
2021-04-11 16:14:27 -0400

    sched/fair: Add feature to toggle SchedTune utilization boosting
    Sometimes we may want to use SchedTune boosting to bias tasks towards
    higher-capacity CPUs without its influence on frequencies, which is
    caused by its utilization-boosting behavior. For example, the CPU
    frequencies may be managed by external frequency-based boosting
    mechanisms.
    
    To better accomodate for such scenarios and potentially reduce power
    usage, add a togglable scheduler feature to control whether the
    scheduler will artificially inflate the effective utilization of tasks
    and CPUs that are under the effect of a SchedTune boost.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    [@0ctobot: Disable SchedTune utilization boosting]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: Salllz <sal235222727@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6818,7 +6818,10 @@ boosted_cpu_util(int cpu, struct sched_walt_cpu_load *walt_load)
 
 	trace_sched_boost_cpu(cpu, util, margin);
 
-	return util + margin;
+	if (sched_feat(SCHEDTUNE_BOOST_UTIL))
+		return util + margin;
+	else
+		return util;
 }
 
 static inline unsigned long
