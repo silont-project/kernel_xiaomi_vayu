2d7f130dc64444718f6e077ff095783a05f11fb7
Sarang Mairal <sarang.mairal@garmin.com>
2021-08-02 19:42:46 +0200

    power: reset: msm-poweroff: lower printk prio for DLOAD,EDLOAD DT props
    Following error level messages are noticed in kernel log:
    >>>>
    [    2.414659] unable to find DT imem DLOAD mode node
    [    2.415149] unable to find DT imem EDLOAD mode node
    <<<<
    
    These correspond to qcom,msm-imem-download_mode and
    qcom,msm-imem-emergency_download_mode. Since these devicetree
    properties are not mandatory, reduce the printk priority
    to debug level.
    
    Change-Id: I5caca9bb755eaa4e9a58514616a67de5079fc7dc
    Signed-off-by: Sarang Mairal <sarang.mairal@garmin.com>
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: TogoFire <italomellopereira@gmail.com>
    Signed-off-by: negrroo <mohammedaelnaggar1@gmail.com>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -609,7 +609,7 @@ static int msm_restart_probe(struct platform_device *pdev)
 
 	np = of_find_compatible_node(NULL, NULL, DL_MODE_PROP);
 	if (!np) {
-		pr_err("unable to find DT imem DLOAD mode node\n");
+		pr_debug("unable to find DT imem DLOAD mode node\n");
 	} else {
 		dload_mode_addr = of_iomap(np, 0);
 		if (!dload_mode_addr)
@@ -618,7 +618,7 @@ static int msm_restart_probe(struct platform_device *pdev)
 
 	np = of_find_compatible_node(NULL, NULL, EDL_MODE_PROP);
 	if (!np) {
-		pr_err("unable to find DT imem EDLOAD mode node\n");
+		pr_debug("unable to find DT imem EDLOAD mode node\n");
 	} else {
 		emergency_dload_mode_addr = of_iomap(np, 0);
 		if (!emergency_dload_mode_addr)
