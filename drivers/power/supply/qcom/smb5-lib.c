d424088114112e0b939496d7ac8983d9eb1d81ab
Salllz <sal235222727@gmail.com>
2022-03-03 09:20:44 +0000

    smb5-lib: silence charging spam
    Signed-off-by: Salllz <sal235222727@gmail.com>

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -3441,7 +3441,7 @@ int smblib_dp_dm(struct smb_charger *chg, int val)
 	u8 stat;
 
 	if (chg->use_bq_pump) {
-		pr_info("dp_dm is controled by our self\n");
+		pr_debug("dp_dm is controled by our self\n");
 		return rc;
 	}
 
